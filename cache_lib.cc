0ac0eef6a07f4867ab3e5461db32c200719a7aee
RCannon <reillycannon@gmail.com>
2020-02-28 13:55:01 -0800

    private Impl, good evictor?

--- cache_lib.cc
+++ cache_lib.cc
@@ -4,7 +4,7 @@
 
 class Cache::Impl 
 {
-  public:
+  private:
     Impl(Cache::size_type maxmem,
         float max_load_factor = 0.75,
         Evictor* evictor = nullptr,
@@ -14,11 +14,17 @@ class Cache::Impl
     Impl& operator=(const Impl&) = delete;
 
     const Cache::size_type maxmem_;
-    Cache::size_type remmem_;
+    int remmem_;
     const float max_load_factor_;
     const Evictor* evictor_;
     const Cache::hash_func hasher_;
     std::unordered_map<key_type, std::pair<Cache::val_type, Cache::size_type>, Cache::hash_func> tbl_;
+  public:
+    void set(key_type key, Cache::val_type val, Cache::size_type size);
+    Cache::val_type get(key_type key, Cache::size_type& val_size) const;
+    bool del(key_type key);
+    Cache::size_type space_used() const;
+    void reset();
 };
 
 Cache::Impl::Impl(Cache::size_type maxmem,
@@ -42,46 +48,75 @@ Cache::~Cache()
 {}
 
 void 
-Cache::set(key_type key, Cache::val_type val, Cache::size_type size)
+Impl::set(key_type key, Cache::val_type val, Cache::size_type size)
 {
-  while (pImpl_->remmem_ - size > pImpl_->maxmem_) 
+  auto evictKey;
+  auto evictSize;
+  while (remmem_ - size <= 0) 
   {
     if (!evictor_) return;
-    
-    return; /* to implement */
+    evictKey = evictor_->evict();
+    evictSize = tbl_.at(evict_key).second;
+    remmem_ += evictSize;
+    std::assert(del(evictKey); 
   }
-  pImpl_->remmem_ = pImpl_->remmem_ - size;
-  pImpl_->tbl_.insert_or_assign(key, std::make_pair(val,size));
+  tbl_.insert_or_assign(key, std::make_pair(val,size));
   return;
 }
 
 Cache::val_type
-Cache::get(key_type key, Cache::size_type& val_size) const
+Impl::get(key_type key, Cache::size_type& val_size) const
 {
-  if (pImpl_->tbl_.find(key) == pImpl_->tbl_.end()) return nullptr;
-  std::pair res = pImpl_->tbl_.at(key);
+  if (tbl_.find(key) == tbl_.end()) return nullptr;
+  std::pair res = tbl_.at(key);
   val_size = res.second;
   return res.first;
 }
 
 bool 
-Cache::del(key_type key)
+Impl::del(key_type key)
 {
-  if (pImpl_->tbl_.erase(key)) return true;
+  if (tbl_.erase(key)) return true;
   return false; 
 }
 
 Cache::size_type 
-Cache::space_used() const
+Impl::space_used() const
 {
-  return pImpl_->maxmem_ - pImpl_->remmem_;
+  return maxmem_ - remmem_;
 }
 
 void
-Cache::reset()
+Impl::reset()
 {
-  pImpl_->tbl_.clear();
-  pImpl_->remmem_ = pImpl_->maxmem_;
+  tbl_.clear();
+  remmem_ = maxmem_;
   return;
+}
+
+/* here are the cache methods, all they do is call the corresponding Impl methods */
+void Cache::set(key_type key, Cache::val_type val, Cache::size_type size)
+{
+  return pImpl_->set(key_type key, Cache::val_type val, Cache::size_type size);
+}
+
+Cache::val_type Cache::get(key_type key, Cache::size_type& val_size) const
+{
+  return pImpl_->get(key_type key, Cache::val_type val, Cache::size_type size);
+}
+
+bool Cache::del(key_type key)
+{
+  return pImpl_->del(key_type key);
+}
+
+Cache::size_type Cache::space_used() const
+{
+  return pImpl_->space_used();
+}
+
+void reset()
+{
+  return pImpl_->reset();
 }
 
