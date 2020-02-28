9b97d6893c4a4cfe9875f67c868c4126e3ecd59e
James McCaull <jamccaull@reed.edu>
2020-02-28 14:45:48 -0800

    Added test for default evictor (no policy)

--- test_cache_lib.cc
+++ test_cache_lib.cc
@@ -129,6 +129,38 @@ void test_reset(){
     assert(c.del(key_1) == false);
     assert(c.get(key_2, val_2_size) == nullptr);
 }
+void test_default_evictor(){
+    // no promises made here by cache.hh
+    // Expected behavior: set should silently fail if insufficient memory remains to store a given value
+    Cache c = Cache(15);
+    std::string key_1 = "Item 1";
+    std::string key_2 = "Item 2";
+    std::string key_3 = "Item 3";
+    const char *val_1 = "3.14159";
+    const char *val_2 = "pi";
+    const char *val_3 = "tau / 2";
+    size_type val_1_size = strlen(val_1) + 1;
+    size_type val_2_size = strlen(val_2) + 1;
+    size_type val_3_size = strlen(val_3) + 1;
+    c.set(key_1, val_1, val_1_size);
+    c.set(key_3, val_3, val_3_size);
+    c.set(key_2, val_2, val_2_size);
+    assert(c.get(key_1, val_1_size) != nullptr);
+    assert(c.get(key_2, val_2_size) != nullptr);
+    assert(c.get(key_3, val_3_size) == nullptr);
+    c.del(key_1);
+    c.set(key_3, val_3, val_3_size);
+    assert(c.get(key_3, val_3_size) != nullptr)
+}
+void test_fifo_evictor(){
+    // Expected behavior for Cache::set when using a fifo evictor:
+    // When not enough memory remains to store a value, the oldest items will be thrown out one by one until
+    // enough space is freed to store the new value.
+    // When attempting to store a value that is too large for the cache to store even with no other space used,
+    // should fail silently without removing anything.
+}
+
+
 int main(){
     /*
      * Notes for future developement: add command line args to disable tests by group
@@ -144,6 +176,8 @@ int main(){
     std::cout << "Passed tests for Cache::space_used" << std::endl;
     test_reset();
     std::cout << "Passed tests for Cache::reset" << std::endl;
+    test_default_evictor();
+    std::cout << "Passed tests for default evictor" << std::endl;
     std::cout << "All unit tests passed!" << std::endl;
     return 0;
 }
